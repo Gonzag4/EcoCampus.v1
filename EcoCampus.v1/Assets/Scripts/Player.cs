8e5f6433616ebd5b29d0464a899072bc316e83c6
Gonzag4 <gonzagaluiz931@outlook.com>
2025-11-07 13:54:27 -0300

    corrida
    acrescentada a animação e a mecânica da corrida

--- EcoCampus.v1/Assets/Scripts/Player.cs
+++ EcoCampus.v1/Assets/Scripts/Player.cs
@@ -1,35 +1,95 @@
 using System.Collections;
-using System.Collections.Generic;   
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using JetBrains.Annotations;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    public float Speed;
-
+    // vou usar o SerializeField para expor a variavel Speed no inspetor do Unity e dexala privada por razões de encapsulamento
+    [SerializeField] private float Speed;
+    [SerializeField] private float runSpeed;
+    // componentes: 
     private Rigidbody2D rig;
+
+    //atributos:
+
+    private float inicialSpeed;
+    private bool _isRunning;
     private Vector2 _direction;
 
+
+    //construtores:
     public Vector2 Direction
     {
         get { return _direction; }
         set { _direction = value; }
     }
-            
+
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+
+
+    //metodos:
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
+        inicialSpeed = Speed;
     }
 
     
     private void Update()
     {
-        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+       onInput();
+
+       onRun();
+
     }
 
     private void FixedUpdate()
     {
-        rig.MovePosition(rig.position + _direction * Speed * Time.fixedDeltaTime);
+        onMove();
+
+
     }
+
+    // #region server para organizar um bloco de codigo
+    #region Movement 
+
+    void onInput()
+    {
+        // entrada do player
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+    }
+
+        void onMove()
+        {
+        // movimentação do player
+        rig.MovePosition(rig.position + _direction * Speed * Time.fixedDeltaTime);
+
+    }
+
+    void onRun()
+    {
+        // corrida do player
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Speed = runSpeed;
+            isRunning = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Speed = inicialSpeed;
+            isRunning = false;
+
+        }
+
+    }
+
+    #endregion
 }
--- EcoCampus.v1/Assets/Scripts/PlayerAniim.cs
+++ EcoCampus.v1/Assets/Scripts/PlayerAniim.cs
@@ -16,25 +16,41 @@ public class PlayerAniim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        onMove();
+        onRun();
+
+    }
+
+
+    #region Movement
+    void onMove()
+    {
         if (player.Direction.sqrMagnitude > 0)
         {
-            anim.SetInteger("Transition", 1);
+            anim.SetInteger("Transition", 1); //andando
         }
         else
         {
-            anim.SetInteger("Transition", 0);
+            anim.SetInteger("Transition", 0); //parado
         }
 
         if (player.Direction.x > 0)
         {
             transform.eulerAngles = new Vector2(0, 0);
         }
-       
+
         if (player.Direction.x < 0)
         {
             transform.eulerAngles = new Vector2(0, 180);
-
-
         }
     }
+
+    void onRun()
+    {
+        if (player.isRunning)
+        {
+            anim.SetInteger("Transition", 2); // correndo
+        }
+    }
+    #endregion
 }
