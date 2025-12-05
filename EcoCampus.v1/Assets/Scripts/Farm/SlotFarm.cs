ef905b9d8a941c6cdb7c19a447a295aec3bb2034
Gonzag4 <gonzagaluiz931@outlook.com>
2025-12-05 18:30:32 -0300

    correção de bug & mec. cavar
    correção do bug de caixa de dialogo  e adicionando a mecânica de cavar

---
+++ EcoCampus.v1/Assets/Scripts/Farm/SlotFarm.cs
@@ -0,0 +1,55 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Sprite hole;
+    [SerializeField] private Sprite carrot;
+
+    [SerializeField] private int digAmount; //quantidade de escavação necessária para cavar o buraco
+    private int initialDigAmount;
+
+
+    void Start()
+    {
+        initialDigAmount = digAmount;
+    }
+
+
+    public void OnHit()
+    {
+      
+        digAmount--;
+
+        if (digAmount <= initialDigAmount / 2)
+        {
+            spriteRenderer.sprite = hole;
+        }
+       /* if (digAmount <= 0)
+        {
+            //plantar cenoura
+            spriteRenderer.sprite = carrot;
+
+        }*/
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Dig"))
+        {
+            Debug.Log("cavou");
+            OnHit();
+        }
+
+    }
+
+
+
+    
+    void Update()
+    {
+        
+    }
+}
